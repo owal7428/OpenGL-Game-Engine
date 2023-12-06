9925b7462e4320beaf81400feab462738fcc745f
Owen Allison <owal7428@colorado.edu>
2023-12-06 11:33:15 -0700

    Added velocity and functions to game object class.

--- src/Engine/Objects/GameObject.cpp
+++ src/Engine/Objects/GameObject.cpp
@@ -15,3 +15,24 @@ void GameObject::Move(glm::vec3 newPosition)
         children.at(i) -> Move(newPos);
     }
 }
+
+void GameObject::MoveVelocity(glm::vec3 newVelocity)
+{
+    glm::vec3 deltaVelocity = newVelocity - velocity;
+
+    velocity = newVelocity;
+
+    int size = children.size();
+    for (int i = 0; i < size; i++)
+    {
+        glm::vec3 oldVelocity = children.at(i) -> getVelocity();
+        glm::vec3 newVel = oldVelocity + deltaVelocity;
+
+        children.at(i) -> MoveVelocity(newVel);
+    }
+}
+
+void GameObject::Update(double deltaTime)
+{
+    Move(position + glm::vec3(velocity.x * deltaTime, velocity.y * deltaTime, velocity.z * deltaTime));
+}
--- src/Engine/Objects/GameObject.hpp
+++ src/Engine/Objects/GameObject.hpp
@@ -7,17 +7,27 @@ class GameObject
 {
 protected:
     glm::vec3 position;
+    glm::vec3 velocity;
 
     std::vector<GameObject*> children;
 
 public:
+    GameObject() {velocity = glm::vec3(0,0,0);}
+
     /* Sets the position vector to new vector
     *  @param newPosition new vec3 to set position to. */
     void Move(glm::vec3 newPosition);
 
+    /* Sets the velocity vector to new vector
+    *  @param newVelocity new vec3 to set velocity to. */
+    void MoveVelocity(glm::vec3 newVelocity);
+
+    void Update(double deltaTime);
+
     void AddChild(GameObject* object) {children.push_back(object);}
 
     inline glm::vec3 getPosition() {return position;}
+    inline glm::vec3 getVelocity() {return velocity;}
 };
 
 #endif // GAME_OBJECT_H
